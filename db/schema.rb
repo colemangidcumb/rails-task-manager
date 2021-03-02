8e923d16f3a1054937e2defa687b01d33a20fd65
coleman gidcumb <colemangidcumb@gmail.com>
2021-03-02 04:24:01 -0700

    db fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_104917) do
+ActiveRecord::Schema.define(version: 2021_03_02_112236) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.string "string"
-    t.string "details"
-    t.string "text"
-    t.string "completed"
-    t.string "boolean"
+    t.text "details"
+    t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
